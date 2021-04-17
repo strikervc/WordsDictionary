9fa10edc4ef7f9ad3d25e0cdfa2d6fcd79b4f6aa
Victor Valdez <victor334_@hotmail.com>
2021-04-16 23:26:28 -0400

    SearchPage remastered, unnecesary views deleted and code refactored

--- WordsDictionary/Views/AboutPage.xaml.cs
+++ WordsDictionary/Views/AboutPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace UIXamarin.Views
+namespace WordsDictionary.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AboutPage : ContentPage
