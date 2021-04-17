0eb1bef98c3b31752c4216c8bd7c9328b532452d
Osv04 <oscar.veloz4@gmail.com>
2021-04-17 00:57:08 -0400

    EntryEffects en todas las paginas

--- WordsDictionary/Views/AboutPage.xaml.cs
+++ WordsDictionary/Views/AboutPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UIXamarin.Views;
+using WordsDictionary.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
