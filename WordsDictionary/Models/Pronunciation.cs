b22e352beab32bcbc2a161fdadbba318dba22d1b
Victor Valdez <victor334_@hotmail.com>
2021-04-17 03:03:17 -0400

    Changes saved on Views

---
+++ WordsDictionary/Models/Pronunciation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace WordsDictionary.Models
+{
+    public class Pronunciation
+    {
+        public string Word { get; set; }
+        public PronunciationObject Pronunciations { get; set; }
+        
+    }
+
+    public class PronunciationObject
+    {
+        public string All { get; set; }
+
+
+    }
+
+
+}
+
