f79b54c74a6d17e6d7b81bf621803bae8b88ef48
Victor Valdez <victor334_@hotmail.com>
2021-04-17 03:55:54 -0400

    Messages.cs created for abstracting messages from dialog pages

---
+++ WordsDictionary/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordsDictionary
+{
+    public static class Messages
+    {
+        public static string OkOption = "Ok";
+
+        public static string NoConnection = "Network error";
+
+        public static string NoConnectionMessage = "There is no internet connection";
+
+        public static string Search = "Search completed";
+
+        public static string Category = "Category";
+
+        public static string Synonym = "Synonym";
+    }
+}
